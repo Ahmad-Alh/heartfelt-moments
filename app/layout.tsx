417de3689466b964a0700d9f16620fa848f78580
Ahmed <ahmed.alhadhrami179@gmail.com>
2025-02-12 01:16:34 +0400

    Updated layout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,20 @@
-import type { Metadata } from 'next'
-import './globals.css'
+import type { Metadata } from "next";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-}
+  title: "Heartfelt Moments",
+  description:
+    "Event Management in the UAE - Birthdays, Gatherings,Parties & Corporate Events",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
